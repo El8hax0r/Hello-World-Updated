1b712af964e3487efa6ce54c1766f7e5e3b86eaa
Matthew Mitchell <matthewprestonmitchell@gmail.com>
2020-04-12 12:15:18 -0700

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+
+namespace HelloWorldUpdated
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {   
+        //user property added
+        private Models.User user = new Models.User();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //the following adds Name context
+            uxName.DataContext = user;
+            uxNameError.DataContext = user;
+            //then add the container context
+            uxContainer.DataContext = user;
+        }
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            //if text in both uxName and uxPassword, allow submit
+            if ((user.Name != string.Empty) && (user.Password != string.Empty))
+            {
+                MessageBox.Show("Submitting password:" + uxPassword.Text);
+            }
+            else
+            {
+                MessageBox.Show("You have not entered both username and password...");
+            }
+        }
+    }
+}
---
+++ WpfApp1/PanelWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+
+namespace HelloWorldUpdated
+{
+    /// <summary>
+    /// Interaction logic for PanelWindow.xaml
+    /// </summary>
+    public partial class PanelWindow : Window
+    {
+        public PanelWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
